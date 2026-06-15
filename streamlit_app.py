283e4d825406f9e394f1223eb997562b0f396513
Flussziege <152208545+Flussziege@users.noreply.github.com>
2026-06-15 08:59:10 +0200

    dsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
+[theme]
+primaryColor = "#6C63FF"
+backgroundColor = "#0E1117"
+secondaryBackgroundColor = "#1A1D27"
+textColor = "#FAFAFA"
+font = "sans serif"
+
 st.title("Geohistorisches Quiz")
 st.write(
     "Errate die historische Figur anhand von Geburts und Todesdaten!"
 )
+
+with st.expander("Anleitung"):
+    st.write('''
+        Mit Spielbeginn wird eine Karte angezeigt, auf der mit grün der Geburtsort und mit rot der Todesort einer
+             historischen Figur markiert ist. Zusätzlich sind die entsprechenden Jahre angegeben.
+        Ziel ist es aus diesen Informationen soll die Figur korrekt erraten werden.
+    ''')
