44c594aed4c64ca5028f9bca88b9c777eac6c29a
Flussziege <152208545+Flussziege@users.noreply.github.com>
2026-06-15 09:05:42 +0200

    dfgfgfg

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,20 @@ st.write(
     "Errate die historische Figur anhand von Geburts und Todesdaten!"
 )
 
-with st.expander("Anleitung"):
-    st.write('''
-        Mit Spielbeginn wird eine Karte angezeigt, auf der mit grün der Geburtsort und mit rot der Todesort einer
-             historischen Figur markiert ist. Zusätzlich sind die entsprechenden Jahre angegeben.
-        Ziel ist es aus diesen Informationen soll die Figur korrekt erraten werden.
-    ''')
+with center:
+    with st.expander("Anleitung"):
+        st.write('''
+            Mit Spielbeginn wird eine Karte angezeigt, auf der mit grün der Geburtsort und mit rot der Todesort einer
+                historischen Figur markiert ist. Zusätzlich sind die entsprechenden Jahre angegeben.
+            Ziel ist es aus diesen Informationen soll die Figur korrekt erraten werden.
+        ''')
 
 
     clicked = st.button(
-        "Start",
+        "🚀 Starten",
         type="primary",
         width="stretch"
     )
+
+if clicked:
+    st.success("Button wurde geklickt!")
